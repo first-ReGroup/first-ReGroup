988d002ba5c6d459539cfacdecc66b477223ef18
first-ReGroup <114463977+first-ReGroup@users.noreply.github.com>
2022-09-27 13:21:09 +0800

    Add files via upload

---
+++ styles/_vti_cnf/slider.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|28 Jun 2021 10:10:01 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|News.html Publications.html index.html People.html Research.html Openings.html
+vti_author:SR|CITYUMD\\chjiang
+vti_modifiedby:SR|CITYUMD\\chjiang
+vti_timecreated:TR|22 Jun 2021 10:54:21 -0000
+vti_cacheddtm:TX|28 Jun 2021 10:10:01 -0000
+vti_filesize:IR|14871
